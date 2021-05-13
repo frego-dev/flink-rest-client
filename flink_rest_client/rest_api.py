9b2ca8010481a86d5f454093f59628ddaf2af404
Attila Mátyás Nagy <anagy@hit.bme.hu>
2021-05-13 17:11:59 +0200

    updated client

--- flink_rest_client/rest_api.py
+++ flink_rest_client/rest_api.py
@@ -177,6 +177,181 @@ class FlinkRestClientV1:
         """
         return self._execute_rest_request(url=f'/jars/{jar_id}', http_method='DELETE')
 
+    def get_jobmanager_config(self):
+        """
+        Returns the cluster configuration.
+
+        Returns
+        -------
+        dict
+            Query result as a dict.
+        """
+        return self._execute_rest_request(url='/jobmanager/logs', http_method='GET')
+
+    def get_jobmanager_metrics(self):
+        """
+        Provides access to job manager metrics.
+
+        Returns
+        -------
+        dict
+            Query result as a dict.
+        """
+        return self._execute_rest_request(url='/jobmanager/metrics', http_method='GET')
+
+    def submit_job(self, params):
+        """
+        Submits a job. This call is primarily intended to be used by the Flink client. This call expects a
+        multipart/form-data request that consists of file uploads for the serialized JobGraph, jars and distributed
+        cache artifacts and an attribute named "request" for the JSON payload.
+
+        Returns
+        -------
+        dict
+            Query result as a dict.
+        """
+        # TODO: it is not clear for me, that what should be attached to the request
+        return self._execute_rest_request(url='/jobs', http_method='POST')
+
+    def get_jobs(self):
+        """
+        Returns an overview over all jobs and their current state.
+
+        Returns
+        -------
+        dict
+            Query result as a dict.
+        """
+        return self._execute_rest_request(url='/jobs', http_method='GET')
+
+    def get_job(self, job_id):
+        """
+        Returns details of a job.
+
+        Parameters
+        ----------
+        job_id: str
+            32-character hexadecimal string value that identifies a job.
+
+        Returns
+        -------
+        dict
+            Query result as a dict.
+        """
+        return self._execute_rest_request(url=f'/jobs{job_id}', http_method='GET')
+
+    def get_job_accumulators(self, job_id):
+        """
+        Returns the accumulators for all tasks of a job, aggregated across the respective subtasks.
+
+        Parameters
+        ----------
+        job_id: str
+            32-character hexadecimal string value that identifies a job.
+
+        Returns
+        -------
+        dict
+            Query result as a dict.
+        """
+        return self._execute_rest_request(url=f'/jobs{job_id}/accumulators', http_method='GET')
+
+    def get_job_checkpoint_statistics(self, job_id, checkpoint_id=None, vertex_id=None):
+        """
+        Returns checkpointing statistics for a job. If the checkpoint_id parameter is also provided, then it returns
+        details for the referenced checkpoint.
+
+        If the checkpoint_id parameter AND vertex_id are provided, it returns checkpoint statistics for a task and its
+        subtasks.
+
+        Parameters
+        ----------
+        job_id: str
+            32-character hexadecimal string value that identifies a job.
+        checkpoint_id: int
+            Int value that identifies a checkpoint.
+        vertex_id:str
+            32-character hexadecimal string value that identifies a job vertex.
+
+        Returns
+        -------
+        dict
+            Query result as a dict.
+        """
+        if checkpoint_id is not None and vertex_id is not None:
+            return self._execute_rest_request(
+                url=f'/jobs/{job_id}/checkpoints/details/{checkpoint_id}/subtasks/{vertex_id}',
+                http_method='GET')
+        elif checkpoint_id is not None:
+            return self._execute_rest_request(
+                url=f'/jobs/{job_id}/checkpoints/details/{checkpoint_id}',
+                http_method='GET')
+        else:
+            return self._execute_rest_request(url=f'/jobs/{job_id}/checkpoints', http_method='GET')
+
+    def get_job_checkpoint_configuration(self, job_id):
+        """
+        Returns the checkpointing configuration.
+
+        Parameters
+        ----------
+        job_id: str
+            32-character hexadecimal string value that identifies a job.
+
+        Returns
+        -------
+        dict
+            Query result as a dict.
+        """
+        return self._execute_rest_request(url=f'/jobs{job_id}/checkpoints/config', http_method='GET')
+
+    def get_jobs_metrics(self, metrics=None, aggs=None, job_ids=None):
+        """
+        Provides access to aggregated job metrics.
+
+        Parameters
+        ----------
+        metrics: list
+            List of string values to select specific metrics.
+        aggs: list
+            List of aggregation modes which should be calculated. Available aggregations are: "min,
+            max, sum, avg".
+        job_ids: list
+            List of 32-character hexadecimal strings to select specific jobs.
+
+        Returns
+        -------
+        dict
+            Query result as a dict.
+        """
+        return self._execute_rest_request(url='/jobs/metrics', http_method='GET')
+
+    def get_jobs_overview(self):
+        """
+        Returns an overview over all jobs.
+
+        Returns
+        -------
+
+        """
+        return self._execute_rest_request(url='/jobs/overview', http_method='GET')
+
+    def terminate_job(self, job_id):
+        """
+        Terminates a job.
+
+        Parameters
+        ----------
+        job_id: str
+            32-character hexadecimal string value that identifies a job.
+
+        Returns
+        -------
+        dict
+            Query result as a dict.
+        """
+        return self._execute_rest_request(url=f'/jobs{job_id}', http_method='PATCH', accepted_status_code=202)
+
     def _execute_rest_request(self, url, http_method, accepted_status_code=None, files=None):
 
         # If accepted_status_code is None then default value is set.
