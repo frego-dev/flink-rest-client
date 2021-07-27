93656f9fc88b92fa6cf0d63f5967592819e93ef3
Attila Mátyás Nagy <anagy@hit.bme.hu>
2021-07-27 22:04:26 +0200

    increased version number

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 setup(
     name='flink-rest-client',
-    version='1.0.3',
+    version='1.0.4',
     description='Easy to use Flink REST API client implementation',
     long_description=readme(),
     long_description_content_type='text/markdown',
