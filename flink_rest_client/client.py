0f349b85a0e2fe7374bf208e63fa2aba8d752af3
Ivan Baldinotti <ivan.baldinotti@gmail.com>
2022-12-07 15:51:11 +0100

    modify client.py

--- flink_rest_client/client.py
+++ flink_rest_client/client.py
@@ -6,7 +6,7 @@ VERSIONS = {"v1": FlinkRestClientV1}
 
 class FlinkRestClient:
     @staticmethod
-    def get(host, port=None, version=None, auth=None):
+    def get(host, port=None, version=None, auth=None, verify=None):
         """
         Constructs a new rest client instance.
 
@@ -24,8 +24,8 @@ class FlinkRestClient:
         port = 8081 if port is None else port
         version = "v1" if version is None else version
         auth = None if auth is None else auth
-
+        verify = None if verify is None else verify
         if version not in VERSIONS.keys():
             raise RestException(f"Unknown REST API version: {version}")
         api_client_cls = VERSIONS[version]
-        return api_client_cls(host=host, port=port, auth=auth)
+        return api_client_cls(host=host, port=port, auth=auth, verify=verify)
